6d19c5d845b684fe094df0bea758ed4c64f031f5
Rudra <rudratosh.91@gmail.com>
2021-12-12 18:28:23 +0530

    Update RcsAccount.php

--- app/RcsAccount.php
+++ app/RcsAccount.php
@@ -14,7 +14,7 @@ class RcsAccount extends Model
     static function getAccounts()
   {
    //if super admin
-    if(Auth::user()->id==1 || Auth::user()->id==2)
+    if(Auth::user()->id==1 || Auth::user()->id==2 || Auth::user()->hasPermissionTo('rcs_balance_management'))
         return  DB::table('rcs_accounts')
         ->select(
             'rcs_accounts.user_id',
@@ -38,30 +38,30 @@ class RcsAccount extends Model
         ->groupBy('rcs_accounts.id')
         ->get()
         ->toArray();
-    elseif(Auth::user()->hasPermissionTo('rcs_balance_management'))
-        return  DB::table('rcs_accounts')
-            ->select(
-                'rcs_accounts.user_id',
-                'rcs_accounts.type',
-                'rcs_accounts.balance',
-                'rcs_accounts.validity',
-                'rcs_accounts.created_by',
-                'rcs_accounts.created_at',
-                'rcs_accounts.updated_at',
-                'rcs_balance.credit_remaining',
-                'users.name',
-                'users.mobile_no'
-            )
-            ->leftJoin('rcs_balance', function ($join) {
-                $join->on('rcs_accounts.user_id', '=', 'rcs_balance.user_id');
-            })
-            ->leftJoin('users', function ($join) {
-                $join->on('rcs_accounts.user_id', '=', 'users.id');
-            })
-            ->where('rcs_accounts.created_by', Auth::user()->id)
-            ->groupBy('rcs_accounts.id')
-            ->get()
-            ->toArray(); 
+    // elseif(Auth::user()->hasPermissionTo('rcs_balance_management'))
+    //     return  DB::table('rcs_accounts')
+    //         ->select(
+    //             'rcs_accounts.user_id',
+    //             'rcs_accounts.type',
+    //             'rcs_accounts.balance',
+    //             'rcs_accounts.validity',
+    //             'rcs_accounts.created_by',
+    //             'rcs_accounts.created_at',
+    //             'rcs_accounts.updated_at',
+    //             'rcs_balance.credit_remaining',
+    //             'users.name',
+    //             'users.mobile_no'
+    //         )
+    //         ->leftJoin('rcs_balance', function ($join) {
+    //             $join->on('rcs_accounts.user_id', '=', 'rcs_balance.user_id');
+    //         })
+    //         ->leftJoin('users', function ($join) {
+    //             $join->on('rcs_accounts.user_id', '=', 'users.id');
+    //         })
+    //         ->where('rcs_accounts.created_by', Auth::user()->id)
+    //         ->groupBy('rcs_accounts.id')
+    //         ->get()
+    //         ->toArray(); 
     elseif(Auth::user()->hasPermissionTo('rcs_account_report'))
         return  DB::table('rcs_accounts')
             ->select(
